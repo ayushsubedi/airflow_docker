cd66054172f9f451fa660907916e4fdaf3468deb
Ayush Subedi <ayush.subedi@gmail.com>
2020-10-09 17:20:16 +0545

    add random failures and also pass retries and retry delay to assure task finally passes

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,7 +1,8 @@
 
 from airflow.models import DAG
-from airflow.utils.dates import days_ago
+from airflow.utils.dates import days_ago, timedelta
 from airflow.operators.python_operator import PythonOperator
+import random
 
 args = {
     'owner' : 'ayush',
@@ -17,7 +18,11 @@ def run_this_func1(**context):
 
 def run_this_func2(**contect):
     # print ("hello_dag")
-    print (str(5/0))
+    # print (str(5/0))
+    if (random.random() > .7):
+        print (str(5/0))
+    else:
+        print ("hello_dag2")
 
 def run_this_func3(**context):
     print ("hello_dag3")
@@ -28,19 +33,25 @@ with dag:
     run_this_task1 = PythonOperator(
         task_id = "run_this_task1",
         python_callable = run_this_func1,
-        provide_context = True
+        provide_context = True,
+        retries = 10,
+        retry_delay = timedelta(seconds=5)
     )
 
     run_this_task2 = PythonOperator(
         task_id = "run_this_task2",
         python_callable = run_this_func2,
-        provide_context = True
+        provide_context = True,
+        retries = 10,
+        retry_delay = timedelta(seconds=5)
     )    
 
     run_this_task3 = PythonOperator(
         task_id = "run_this_task3",
         python_callable = run_this_func3,
-        provide_context = True
+        provide_context = True,
+        retries = 10,
+        retry_delay = timedelta(seconds=5)
     )
 
     #DAG
