3c13de3aeebf453626fe2ee08aca86cfefbee612
Ayush Subedi <ayush.subedi@gmail.com>
2020-10-09 16:44:33 +0545

    create a sample failing DAG

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -8,26 +8,40 @@ args = {
     'start_date' : days_ago(1)
 }
 
-dag = DAG(dag_id='sample_dag', default_args=args, schedule_interval=None)
 
-def run_this_func(**context):
+dag = DAG(dag_id='wassap_dag', default_args=args, schedule_interval=None)
+
+
+def run_this_func1(**context):
     print ("hello_dag")
 
-def run_this_func2(**context):
-    print ("hello_dag2")
+def run_this_func2(**contect):
+    # print ("hello_dag")
+    print (str(5/0))
+
+def run_this_func3(**context):
+    print ("hello_dag3")
+
+
 
 with dag:
-    run_this_task = PythonOperator(
-        task_id = "run_this",
-        python_callable = run_this_func,
+    run_this_task1 = PythonOperator(
+        task_id = "run_this_task1",
+        python_callable = run_this_func1,
         provide_context = True
     )
 
     run_this_task2 = PythonOperator(
-        task_id = "run_this2",
+        task_id = "run_this_task2",
         python_callable = run_this_func2,
+        provide_context = True
+    )    
+
+    run_this_task3 = PythonOperator(
+        task_id = "run_this_task3",
+        python_callable = run_this_func3,
         provide_context = True
     )
 
     #DAG
-    run_this_task >> run_this_task2
+    run_this_task1 >> run_this_task2 >> run_this_task3 
